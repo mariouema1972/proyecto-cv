63aa183424ffb6cf5e9a255fd1173267326f2d3e
Mario Uema <65201817+mariouema1972@users.noreply.github.com>
2023-01-29 14:51:49 -0300

    desde githuvdev

---
+++ js/function.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function (){
+    console.log("Capturamos el evento clik");
+    document.getElementById("demo").innerHTML = "Inglés básico, movilidad propia, disponibilidad full time. Salario a convenir"
+}
